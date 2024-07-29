6e04295741daafc2d2ecf59fef59761d4cc48833
AeolusZane <aeoluj@gmail.com>
2024-07-29 15:37:29 +0800

    event

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -5,9 +5,12 @@ import "hardhat/console.sol";
 contract Counter {
     uint counter;
 
+    event CounterInc(address sender, uint counter);
+
     function count() public {
         counter++;
         console.log("Now, counter is: ", counter);
+        emit CounterInc(msg.sender, counter);
     }
 
     function getCount() public view returns (uint) {

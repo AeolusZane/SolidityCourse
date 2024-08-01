72d3e44f731f81ae0c4c32ac0edf0de8e4ef2ebb
AeolusZane <aeoluj@gmail.com>
2024-08-02 01:48:44 +0800

    Update Hero.sol

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -2,12 +2,6 @@ pragma solidity ^0.8.24;
 
 /**
 - 生成随机的英雄属性
-- 用户可以选择他们想要生成的英雄类型
-    - classes：Mage, Healer, Barbarian
-    - Class不影响属性的创建，生成一个稀有属性的【史诗级】英雄概率很小
-- 生成一个英雄支付0.05个ETH
-- 可以通过address访问生成的英雄
-- 英雄存储在链上
 - 英雄属性：strength, health, intellect, magic, dexterity
 - 属性随机生
     - 创建英雄规则，价格：0.05 ETH，支付到founder账号,founder可以更改收款地址. 属性生成，随机生成属性值，值在0 ~ 18，0 ~ 17...，0 ~ 13之间随机 例如，第一次生成Health，值会在0 ~ 18之间随机，第二次生成Intellect，值会在0 ~ 17之间随机...
@@ -24,8 +18,21 @@ contract Hero {
 
     mapping(address => uint[]) addressToHeroes;
 
-    function getHeroes() public view returns (uint[] memory){
+    function getHeroes() public view returns (uint[] memory) {
         return addressToHeroes[msg.sender];
+    }
+
+    function generateRandom() public view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(
+                        block.prevrandao,
+                        block.timestamp,
+                        msg.sender
+                    )
+                )
+            );
     }
 
     function createHero(Class _class) public payable {
@@ -34,5 +41,28 @@ contract Hero {
             msg.value >= 0.05 ether,
             "Please send more money, at least 0.05 ether"
         );
+        // 英雄属性：strength, health, intellect, magic, dexterity
+        uint[] memory stats = new uint[](5);
+
+        stats[0] = 2;
+        stats[1] = 7;
+        stats[2] = 12;
+        stats[3] = 17;
+        stats[4] = 22;
+
+        uint len = 5;
+        uint hero = uint(_class);
+
+        do {
+            uint pos = generateRandom() % len;
+            uint value = (generateRandom() % (13 + len)) + 1;
+
+            hero |= value << stats[pos];
+
+            len--;
+            stats[pos] = stats[len];
+        } while (len > 0);
+
+        addressToHeroes[msg.sender].push(hero);
     }
 }
